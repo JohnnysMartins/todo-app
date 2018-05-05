1ca63f4804167b328c2985b60a5962b7e073522d
JohnnysMartins <johnnys.adson@gmail.com>
2018-05-05 18:35:51 -0300

    Finalizando backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
+const cors = require('./cors')
 
 const port = 3003
 
@@ -8,6 +9,7 @@ const server = express()
 
 server.use(express.urlencoded({ extended: true }))
 server.use(express.json())
+server.use(cors)
 
 server.listen(port, ()=>{
     console.log(`BACK END running on port => ${port}`)
