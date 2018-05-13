6f539f6e5aefd0fc1e189e00a8de0ad3b96da0f0
JohnnysMartins <johnnys.adson@gmail.com>
2018-05-13 17:55:46 -0300

    Criando acoes e adicionando nova atividade na base de dados.

--- frontend/src/todo/todo-form.jsx
+++ frontend/src/todo/todo-form.jsx
@@ -7,11 +7,12 @@ import IconButton from '../template/icon-button'
 export default props => (
     <div role='form' className='form-row'>
         <Grid cols='12 9 10'>
-            <input type="text" id='description' className='form-control' placeholder='Adicione uma tarefa'/>
+            <input type="text" id='description' className='form-control'
+            onChange={props.handleChange} value={props.description} placeholder='Adicione uma tarefa'/>
         </Grid>
 
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'> </IconButton>
+            <IconButton styleClass='primary' icon='plus' onClick={props.handleAdd}> </IconButton>
         </Grid>
     </div>
 )
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import PageHeader from '../template/page-header';
 import TodoForm from './todo-form';
 import TodoList from './todo-list';
 
+const URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({ ...this.state, description: event.target.value })
+    }
+
+    handleAdd() {
+        const description = this.state.description
+        axios.post(URL, { description })
+        .then(res => {
+            console.log('Funcionou!')
+        })
+    }
 
     render() {
         return (
             <div>
-                <PageHeader name='Tarefas' small='Cadastro'/>
-                <TodoForm />
+                <PageHeader name='Tarefas' small='Cadastro' />
+                <TodoForm description={this.state.description} handleChange={this.handleChange} handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
