8560c76791d54b8677392ddff8051cf071a283b1
JohnnysMartins <johnnys.adson@gmail.com>
2018-07-08 17:05:27 -0300

    Ajustando as chamada do backend

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,48 +1,53 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import PageHeader from '../template/page-header';
-import TodoForm from './todo-form';
-import TodoList from './todo-list';
+import React, { Component } from 'react'
+import axios from 'axios'
+import PageHeader from '../template/page-header'
+import TodoForm from './todo-form'
+import TodoList from './todo-list'
 
 const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { description: '', list: [] }
 
-    constructor(props) {
-        super(props)
-        this.state = { description: '', list: [] }
-        
-        this.handleChange = this.handleChange.bind(this)
-        this.handleAdd = this.handleAdd.bind(this)
-        this.setDataList = this.setDataList.bind(this)
-        this.setDataList()
-    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleAdd = this.handleAdd.bind(this)
+    this.setDataList = this.setDataList.bind(this)
+    this.setDataList()
+  }
 
-    setDataList(){
-        axios.get(URL).then(res =>{
-          this.setState({...this.state, list: res.data})
-        })
+  async setDataList() {
+    try {
+      const { data } = await axios.get(URL)
+      this.setState({ ...this.state, list: data })
+    } catch (error) {
+      console.error(error)
     }
+  }
 
-    handleChange(event) {
-        this.setState({ ...this.state, description: event.target.value })
-    }
+  handleChange(event) {
+    this.setState({ ...this.state, description: event.target.value })
+  }
 
-    handleAdd() {
-        const description = this.state.description
-        axios.post(URL, { description })
-        .then(res => {
-            console.log('Funcionou!')
-        })
-    }
+  handleAdd() {
+    const description = this.state.description
+    axios.post(URL, { description }).then(res => {
+      console.log('Funcionou!')
+    })
+  }
 
-    render() {
-        return (
-            <div>
-                <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm description={this.state.description} handleChange={this.handleChange} handleAdd={this.handleAdd} />
-                <TodoList />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <PageHeader name="Tarefas" small="Cadastro" />
+        <TodoForm
+          description={this.state.description}
+          handleChange={this.handleChange}
+          handleAdd={this.handleAdd}
+        />
+        <TodoList />
+      </div>
+    )
+  }
 }
